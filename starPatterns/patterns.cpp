a69fecbc5fa0feba821623d4f7437f4abae7b3e3
CryWolf <malekfaizanmahedi@gmail.com>
2026-04-18 23:06:07 -0300

    123 ladder

--- starPatterns/patterns.cpp
+++ starPatterns/patterns.cpp
@@ -20,9 +20,19 @@ void print2(int n) {
     }
 }
 
+void print3(int n) {
+    for(int i = 1; i <= n; i ++) {
+        for(int j = 1; j <= i; j ++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
     int n;
     cin >> n;
     // print1(n);
-    print2(n);
+    // print2(n);
+    print3(n);
 }
