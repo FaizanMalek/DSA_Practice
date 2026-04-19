0e3bd17a2c7abf7709e5d4331bfa6ccf50f64f24
CryWolf <malekfaizanmahedi@gmail.com>
2026-04-18 23:34:20 -0300

    inverse ladder

--- starPatterns/patterns.cpp
+++ starPatterns/patterns.cpp
@@ -29,10 +29,40 @@ void print3(int n) {
     }
 }
 
+void print4(int n) {
+    for(int i = 1; i <= n; i ++) {
+        for(int j = 1; j <= i; j ++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print5(int n) {
+    for(int i = n; i > 0; i --) {
+        for(int j = i; j > 0; j --) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void print6(int n) {
+    for(int i = n; i > 0; i --) {
+        for(int j = i; j > 0; j --) {
+            cout << i - j + 1 << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main() {
     int n;
     cin >> n;
     // print1(n);
     // print2(n);
-    print3(n);
+    // print3(n);
+    // print4(n);
+    // print5(n);
+    print6(n);
 }
