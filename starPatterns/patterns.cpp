b7affa53840c3ce1d82e9b4e788e096f8755c8e3
CryWolf <malekfaizanmahedi@gmail.com>
2026-04-25 14:55:08 -0300

    right angle number++ triangle

--- starPatterns/patterns.cpp
+++ starPatterns/patterns.cpp
@@ -134,6 +134,18 @@ void print12(int n) {
         cout << endl;
     }
 }
+
+void print13(int n) {
+    int count = 1;
+    for (int i = 1; i <= n; i ++) {
+        for (int j = 1; j <= i; j ++) {
+            cout << count++ << " ";
+        }
+        cout << endl;
+    }
+}
+
+
 int main() {
     int n;
     cin >> n;
@@ -148,5 +160,6 @@ int main() {
     // print9(n);
     // print10(n);
     // print11(n);
-    print12(n);
+    // print12(n);
+    print13(n);
 }
